355bd1af36e00f5e99585be020b40eb2eeed8a52
Your Name <your@email.com>
2026-06-14 21:26:15 +0530

    Added division support and improved Vortex interpreter

--- compilerx.cpp
+++ compilerx.cpp
@@ -7,37 +7,51 @@
 
 using namespace std;
 
-// ---------------- Ttoken struct ----------------
+// ---------------- Token ----------------
 struct Token {
     string type;
     string value;
 };
 
-// ---------------- tokenizer ----------------
+// ---------------- Tokenizer ----------------
 vector<Token> tokenize(string code) {
     vector<Token> tokens;
     int i = 0;
 
     while (i < code.length()) {
+
+        // Skip whitespace
         if (isspace(code[i])) {
             i++;
+            continue;
+        }
+
+        // Skip comments
+        if (code[i] == '/' && i + 1 < code.length() && code[i + 1] == '/') {
+            while (i < code.length() && code[i] != '\n')
+                i++;
             continue;
         }
 
         // Numbers
         if (isdigit(code[i])) {
             string num;
-            while (i < code.length() && isdigit(code[i])) {
+
+            while (i < code.length() &&
+                  (isdigit(code[i]) || code[i] == '.')) {
                 num += code[i];
                 i++;
             }
+
             tokens.push_back({"NUMBER", num});
         }
 
-        // Identifiers / keywords
+        // Keywords / Identifiers
         else if (isalpha(code[i])) {
             string word;
-            while (i < code.length() && isalnum(code[i])) {
+
+            while (i < code.length() &&
+                  (isalnum(code[i]) || code[i] == '_')) {
                 word += code[i];
                 i++;
             }
@@ -52,17 +66,29 @@ vector<Token> tokenize(string code) {
 
         // Operators
         else {
+
             char ch = code[i];
 
-            if (ch == '=') tokens.push_back({"EQUALS", "="});
-            else if (ch == '+') tokens.push_back({"PLUS", "+"});
-            else if (ch == '-') tokens.push_back({"MINUS", "-"});
-            else if (ch == '*') tokens.push_back({"MULTIPLY", "*"});
-            else if (ch == '/') tokens.push_back({"DIVIDE", "/"});
+            if (ch == '=')
+                tokens.push_back({"EQUALS", "="});
+
+            else if (ch == '+')
+                tokens.push_back({"PLUS", "+"});
+
+            else if (ch == '-')
+                tokens.push_back({"MINUS", "-"});
+
+            else if (ch == '*')
+                tokens.push_back({"MULTIPLY", "*"});
+
+            else if (ch == '/')
+                tokens.push_back({"DIVIDE", "/"});
+
             else {
                 cout << "Unexpected character: " << ch << endl;
                 exit(1);
             }
+
             i++;
         }
     }
@@ -70,71 +96,115 @@ vector<Token> tokenize(string code) {
     return tokens;
 }
 
-// ---------------- interpreter ----------------
-int evaluate(vector<string> expr, map<string, int>& vars) {
+// ---------------- Expression Evaluator ----------------
+double evaluate(vector<string> expr,
+                map<string, double>& vars) {
+
     vector<string> processed;
 
     for (auto& token : expr) {
+
         if (vars.count(token))
             processed.push_back(to_string(vars[token]));
         else
             processed.push_back(token);
     }
 
-    // VERY simple evaluation (left to right)
-    int result = stoi(processed[0]);
+    if (processed.empty()) {
+        cout << "Empty expression!" << endl;
+        exit(1);
+    }
+
+    double result = stod(processed[0]);
 
     for (int i = 1; i < processed.size(); i += 2) {
-        string op = processed[i];
-        int val = stoi(processed[i + 1]);
 
-        if (op == "+") result += val;
-        else if (op == "-") result -= val;
-        else if (op == "*") result *= val;
-        else if (op == "/") result /= val;
+        string op = processed[i];
+        double val = stod(processed[i + 1]);
+
+        if (op == "+")
+            result += val;
+
+        else if (op == "-")
+            result -= val;
+
+        else if (op == "*")
+            result *= val;
+
+        else if (op == "/") {
+
+            if (val == 0) {
+                cout << "Division by zero error!" << endl;
+                exit(1);
+            }
+
+            result /= val;
+        }
     }
 
     return result;
 }
 
-// ---------------- main ----------------
+// ---------------- Main ----------------
 int main() {
+
     ifstream file("program.vortex");
+
+    if (!file.is_open()) {
+        cout << "Could not open program.vortex" << endl;
+        return 1;
+    }
+
     stringstream buffer;
     buffer << file.rdbuf();
+
     string code = buffer.str();
 
     vector<Token> tokens = tokenize(code);
 
-    map<string, int> variables;
+    map<string, double> variables;
 
     for (int i = 0; i < tokens.size(); i++) {
 
-        // let x = 5
+        // let x = 10
         if (tokens[i].type == "LET") {
+
+            if (i + 3 >= tokens.size()) {
+                cout << "Invalid variable declaration" << endl;
+                return 1;
+            }
+
             string var = tokens[i + 1].value;
-            int value = stoi(tokens[i + 3].value);
+            double value = stod(tokens[i + 3].value);
+
             variables[var] = value;
+
             i += 3;
         }
 
-        // show x + y
+        // show expression
         else if (tokens[i].type == "SHOW") {
+
             vector<string> expr;
+
             i++;
 
-            while (i < tokens.size() && tokens[i].type != "LET" && tokens[i].type != "SHOW") {
+            while (
+                i < tokens.size() &&
+                tokens[i].type != "LET" &&
+                tokens[i].type != "SHOW"
+            ) {
                 expr.push_back(tokens[i].value);
                 i++;
             }
 
-            i--; // step back
-            int result = evaluate(expr, variables);
-            cout << "Output will be " << result << endl;
+            i--;
+
+            double result = evaluate(expr, variables);
+
+            cout << "Output: " << result << endl;
         }
     }
 
     return 0;
 }
-
-
